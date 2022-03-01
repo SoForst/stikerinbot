57a7cd27d20d0e7ae82e507d9f72a2fb6dcfdda0
SoForst <96701479+SoForst@users.noreply.github.com>
2022-03-02 06:55:56 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, fla + 'donasi', `
 ┌「 *Donasi • Emoney* 」
-├ https://saweria.co/ariffb
-├ https://trakteer.id/ariffb/tip
+├ https://saweria.co/zenn
+├ https://trakteer.id/zenn/tip
 └────
 `.trim(), '© stikerin', 'Donasi', '.donasi', m)
 handler.help = ['donasi']
